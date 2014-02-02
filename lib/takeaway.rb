535897bf481236c1ab27508e5550e50cc3e32481
Ayazg <ayaz0206@gmail.com>
2014-02-02 23:59:04 +0000

    Implemented Twilio

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'twilio-ruby'
+
 class Takeaway
 
 		def menu
@@ -11,11 +14,44 @@ class Takeaway
 		]
 		end
 
-		attr_accessor :order
+		attr_accessor :order, :total
 
 		def initialize
 			@order = []
+			@total = []
 		end
 
+		def take_order(item, qty, price)
+			raise "Oops, sorry, something is wrong!" 
+			raise "Oops, the total seems to be wrong!" 
+			order << item
+			total << qty * price
+			send_text
+
+		end
+
+		# def right_total?(item, qty, price)
+		# end
+
+		# def right_item?(item)
+		# 	menu.any? {|menu| menu[:dish] == item.capitalize}
+		# end
+
+		def send_text(message)
+			# Get your Account Sid and Auth Token from twilio.com/user/account
+				account_sid = 'ACadcee7ea3c188113ae1947dfafa4865a'
+				auth_token = '5ebcdfdbb5bc6abfe935e1e0aa52ad1d'
+				@client = Twilio::REST::Client.new account_sid, auth_token
+				message = @client.account.messages.create(
+		    :to => "+447427053654",
+		    :from => "+441988222006",
+		    :body => "Thank you! Your order was placed and will be delivered soon!")
+				puts message.sid
+		end
 
 end
+
+
+
+
+
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -12,5 +12,22 @@ describe Takeaway do
 			expect{menu.take_order("Shwarama", 2, 9.00)}.to be_true
 		end
 
+		# it "should not throw an error if item ordered is on the menu" do
+		# 	expect(menu.take_order("Chips", 1, 1.50)).to_not raise_error
+		# end
+
+		# it "should throw an error if item ordered is not on the menu" do
+		# 	expect(menu.take_order("Coke",1, 0.60 )).to raise_error(RuntimeError)
+		# end
+
+		# it "should throw an error if the price is not correct" do
+		# 	expect(menu.take_order("Tabbouleh", 2, 5.00)).to raise_error(RuntimeError)
+		# end
+
+		it "should send a confirmation text for a successful order" do
+			allow(menu).to receive(:send_text)
+		end
+
+
 
 end
