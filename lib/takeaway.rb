f61ed443706197e7edacbafa88cdf6dcbd779a7b
Ayazg <ayaz0206@gmail.com>
2014-02-02 20:39:04 +0000

    Added menu - List of dishes

---
+++ lib/takeaway.rb
@@ -0,0 +1,16 @@
+class Takeaway
+
+		def menu
+			[{dish: "Hommos", price: 2.50 },
+						{dish: "Tabbouleh", price: 3.00 },
+						{dish: "Shwarama", price: 4.00 },
+						{dish: "Chips", price: 1.50 },
+						{dish: "Roasted Chicken", price: 7.50 },
+						{dish: "King Prawns", price: 8.00 },
+						{dish: "Falafel", price: 2.50 },
+		]
+		end
+
+		
+
+end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,12 @@
+require 'takeaway'
+
+describe Takeaway do
+
+	let(:menu) { Takeaway.new }
+
+		it "should have a menu" do
+			expect(menu).to eq(menu)
+		end	
+
+
+end
